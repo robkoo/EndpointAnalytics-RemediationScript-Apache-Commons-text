eb73166694c6b4329870f704f150219afa3261cf
robkoo <robkoo@users.noreply.github.com>
2023-04-25 22:59:08 +0200

    Add files via upload

---
+++ Detect-Apache-Commons-text.ps1
@@ -0,0 +1,25 @@
+
+$Debug = 0
+
+$Folder = $env:APPDATA + '\JetBrains\'
+If ($Debug) {
+    $Folder = 'C:\Users\RobKooiman\OneDrive - MijnBedrijf365\Script\PowerShell\Intune\EndpointAnalyticsRemediationScripts\Commons-text\Test\'
+}
+
+Write-Host $Folder -ForegroundColor Yellow
+
+If (Test-Path $Folder) {
+    $Folders = Get-ChildItem -Path ($Folder + "Rider20*.*") -Directory | Sort-Object  Name -Desc
+
+    If ($Debug) {
+        Write-Host "DEBUG" $Folders.Count
+    }
+
+    If ($Folders.Count -gt 1) {
+        Write-Host "$($Folders.Count - 1) old folders found"
+        Exit 1
+    } Else {
+        Write-Host "No old folders found"
+        Exit 0
+    }
+}
---
+++ Remediate-Apache-Commons-text.ps1
@@ -0,0 +1,33 @@
+
+$Debug = 0
+
+$Folder = $env:APPDATA + '\JetBrains\'
+#If ($Debug) {
+    $Folder = 'C:\Users\RobKooiman\OneDrive - MijnBedrijf365\Script\PowerShell\Intune\EndpointAnalyticsRemediationScripts\Commons-text\Test\'
+#}
+
+If ($Debug) {
+    Write-Host $Folder -ForegroundColor Yellow
+}
+
+If (Test-Path $Folder) {
+    $Folders = Get-ChildItem -Path ($Folder + "Rider20*.*") -Directory | Sort-Object  Name -Desc
+
+    If ($Folder) {
+        If ($Debug) {
+            $Folders
+        }
+        $Highest = $Folders[0].Name
+        If ($Debug) {
+            Write-Host "DEBUG Highest" $Highest
+        }
+        $Folders | ForEach-Object {
+            If ($_.Name -ne $Highest) {
+                Write-Host "DEBUG Remove Folder" $_.FullName
+                If (-Not $Debug) {
+                    Remove-Item -LiteralPath $_.FullName -Force -Recurse
+                }
+            }
+        }
+    }
+}
